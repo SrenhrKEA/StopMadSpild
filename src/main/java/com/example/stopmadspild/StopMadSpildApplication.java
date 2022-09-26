f59a8c36ed79370bea3b8e910c70c09563209a4e
CodeClod <mika9103@stud.kea.dk>
2022-09-26 11:21:37 +0200

    Base Version

---
+++ src/main/java/com/example/stopmadspild/StopMadSpildApplication.java
@@ -0,0 +1,13 @@
+package com.example.stopmadspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopMadSpildApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StopMadSpildApplication.class, args);
+  }
+
+}
